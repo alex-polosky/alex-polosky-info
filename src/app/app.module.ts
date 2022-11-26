9b499a013691fec706d8e133e9877b6330e95651
Alex <alexpolosky@gmail.com>
2022-11-26 00:48:05 -0500

    Updated layout details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { LayoutModule } from '@angular/cdk/layout';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -35,10 +37,12 @@ import { NgModule } from '@angular/core';
     BrowserModule,
     HttpClientModule,
     LayoutModule,
+    MatCardModule,
     MatGridListModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatProgressSpinnerModule,
     MatSortModule,
     MatTableModule,
     MatToolbarModule
